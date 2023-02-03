a4abbd33b6f610d2d31bef0e69ed43ddde34271c
avitanthetaa <avi@tanthetaa.co.in>
2023-02-03 15:50:40 +0530

    ogs to public

--- components/MintNFTs.js
+++ components/MintNFTs.js
@@ -273,7 +273,7 @@ export const MintNFTs = ({ onClusterChange }) => {
     const { nft } = await metaplex.candyMachines().mint({
       candyMachine,
       collectionUpdateAuthority: candyMachine.authorityAddress,
-      group: "Public",
+      group: "OGs",
     });
 
     setNft(nft);
